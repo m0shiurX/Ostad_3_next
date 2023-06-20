0841c8a492442a739eea7cd132f4e2521398c93a
Moshiur Rahman <unimrgm@gmail.com>
2023-06-20 23:15:06 +0600

    2. a

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 // 1. a. A date function that returns current day of the week
 
 function getDayOfWeek(dateString) {
-  const date = new Date(dateString);
-  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  const dayOfWeekIndex = date.getDay();
-  return daysOfWeek[dayOfWeekIndex];
+	const date = new Date(dateString);
+	const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	const dayOfWeekIndex = date.getDay();
+	return daysOfWeek[dayOfWeekIndex];
 }
 
 // 1. b. implementation of the function to display the day of current date
 const date = new Date();
 const dayOfWeek = getDayOfWeek(date);
-console.log("\n Today is ", dayOfWeek);
+console.log('\n Today is ', dayOfWeek);
 
+// 2. a.
+function squareRootOfSumOfNumbersArray(numbers) {
+	const sumOfNumbersArray = numbers.reduce((sum, num) => sum + Math.pow(num, 2), 0);
+	const squareRoot = Math.sqrt(sumOfNumbersArray);
+	return squareRoot;
+}
 
-
-// 2. a. 
+// 2. b.
+const numbers = [3, 8, 3, 4, 5];
+const sqroot = squareRootOfSumOfNumbersArray(numbers);
+console.log('\n The square root of given numbers is ', sqroot);
